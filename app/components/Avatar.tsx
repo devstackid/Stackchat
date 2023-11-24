bc0e893a7798ae5dd7a570d391469d959dfc47e0
muhsyaifudin51 <itsmedynnn@gmail.com>
2023-11-24 16:09:11 +0700

    fitur live chat pusherjs dan selesai

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,6 +3,7 @@
 import { User } from '@prisma/client'
 import Image from 'next/image'
 import React from 'react'
+import useActiveList from '../hooks/useActiveList'
 
 interface AvatarProps {
     user?: User
@@ -11,6 +12,8 @@ interface AvatarProps {
 const Avatar: React.FC<AvatarProps> = ({
     user
 }) => {
+    const {members} = useActiveList()
+    const isActive = members.indexOf(user?.email!) != -1
   return (
     <div className='relative'>
         <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
@@ -20,22 +23,24 @@ const Avatar: React.FC<AvatarProps> = ({
                 fill
             />
         </div>
-        <span 
-        className='
-            absolute
-            black
-            rounded-full
-            bg-green-500
-            ring-2
-            ring-white
-            top-0
-            right-0
-            h-2
-            w-2
-            md:h-3
-            md:w-3
-        '
-        />
+        {isActive && (
+            <span 
+            className='
+                absolute
+                black
+                rounded-full
+                bg-green-500
+                ring-2
+                ring-white
+                top-0
+                right-0
+                h-2
+                w-2
+                md:h-3
+                md:w-3
+            '
+            />
+        )}
             
     </div>
   )
